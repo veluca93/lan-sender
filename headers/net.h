6faafa19c1896dfee59a87991321cf9549987ea8
Luca Versari <veluca93@gmail.com>
2014-12-07 11:25:38 +0100

    Used the right definition for ip_t and port_t

--- headers/net.h
+++ headers/net.h
@@ -1,8 +1,9 @@
 #ifndef LS_NET_H
 #define LS_NET_H
 #include <stdint.h>
+#include <netinet/in.h>
 
-typedef uint32_t ip_t;
-typedef uint16_t port_t;
+typedef struct in_addr ip_t;
+typedef in_port_t port_t;
 
 #endif
